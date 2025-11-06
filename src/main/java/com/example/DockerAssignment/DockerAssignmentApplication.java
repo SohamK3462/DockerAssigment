85a88df48781c895fb06c3b5170bf93329600f0b
SOHAM-K_hdfclife <soham.k@hdfclife.com>
2025-11-06 16:25:20 +0530

    Initial Commit

---
+++ src/main/java/com/example/DockerAssignment/DockerAssignmentApplication.java
@@ -0,0 +1,22 @@
+package com.example.DockerAssignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DockerAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerAssignmentApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String helloDocker() {
+		return "Hello Docker!";
+	}
+
+}
